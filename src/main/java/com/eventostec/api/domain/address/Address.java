2ca1c99df156117e0ddd924d9beaa9cb352123fb
bruno.tito <brunosantostito2@hotmail.com>
2025-07-27 16:51:04 -0300

    feat: criação da api

--- src/main/java/com/eventostec/api/domain/address/Address.java
+++ src/main/java/com/eventostec/api/domain/address/Address.java
@@ -3,11 +3,20 @@ package com.eventostec.api.domain.address;
 import com.eventostec.api.domain.event.Event;
 import jakarta.persistence.*;
 import jdk.jfr.Enabled;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.UUID;
 
 @Table
 @Enabled
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address {
     @Id
     @GeneratedValue
